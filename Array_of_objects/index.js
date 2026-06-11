6017bd886f5b5a2007c008a7a7511f9fd9447d63
chandrashekhar kavishetti <chandrukavishetti26@gmail.com>
2026-06-12 01:06:50 +0530

    feat : map,filter,reduce etc..

--- Array_of_objects/index.js
+++ Array_of_objects/index.js
@@ -1,10 +1,47 @@
 const fruits = [{ name: "apple", color: "red", calories: 95 },
 { name: "orange", color: "orange", calories: 105 }];
 
-console.log(fruits[0].name);
-console.log(fruits[1].calories);
+// console.log(fruits[0].name);
+// console.log(fruits[1].calories);
 
-fruits.push({name:"grapes",color:"purple",calories:62});
-console.log(fruits);
-fruits.pop();
-console.log(fruits);
+// fruits.push({name:"grapes",color:"purple",calories:62});
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+
+// //for each
+// fruits.forEach(fruits=>console.log(fruits.calories));
+
+// //map
+// const fruitsName=fruits.map(fruits=>fruits.name);
+// const fruitsColor=fruits.map(fruits=>fruits.color);
+
+// console.log(fruitsName);
+// console.log(fruitsColor);
+
+// //filter()
+// const yellowFruits=fruits.filter(fruit=>fruit.color==="yellow");
+// const lowCalFruits=fruits.filter(fruit=>fruit.calories<100);
+
+// console.log(lowCalFruits);
+
+//reduse
+const maxFruit=fruits.reduce((max,fruit)=>fruit.calories>max.calories? fruit:max);
+
+console.log(maxFruit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
